fd73792593bfa3856799b0312620b1c02aef0341
Arturo Avila <mysticmetromx@gmail.com>
2021-04-22 20:35:39 -0500

    Proyectos con manim
    Códigos de las animaciones para mis clases

---
+++ garbage.py
@@ -0,0 +1,5 @@
+print("{0:8} | {1:6}".format("Fruit", "Quantity"))
+print("{0:8} | {1:8}".format("Apples", 3.0))
+print("{0:8} | {1:9}".format("Oranges", 10))
+
+print("This is my ten-character, two-decimal number: %5.2f" % (13.579))
